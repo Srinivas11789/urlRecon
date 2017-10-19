52aac51becc22f52bb3e8bb90f130e0729fcbef1
Srinivas <spg349@nyu.edu>
2017-10-19 00:11:29 -0400

    rest api

--- modules/restApi.py
+++ modules/restApi.py
@@ -1,24 +1,50 @@
-#############################################################################################
+######################################################################################
 #
 #
-# 	 				REST API DRIVERS
+# 	 				                   REST API DRIVERS
 #
-# 				AUTHOR: SRINIVAS PISKALA GANESH BABU
+# 				              AUTHOR: SRINIVAS PISKALA GANESH BABU
 #
-#				DESCRIPTION:
+#				      DESCRIPTION:
 #
 #
 #
-#				FUNCTIONS:
-#
-##############################################################################################
-
+#			          FUNCTIONS:
+#                           * getRequest
+#                           * postRequest
+#######################################################################################
 # Import Statements for Libraries
 import requests # Rest Api Library
 
+# Handle to make HTTP GET Request and return a JSON output
+def getRequest(url, headers=None, auth=None, ):
+    if headers:
+        response = requests.get(url, headers =headers)
+    response = requests.get(url)
+    try:
+        return response.json()
+    except ValueError as e:
+        if str(e).lower().strip() == "no json object could be decoded":
+            return response.text
+        else:
+            print "Get Request to the url "+url+" failed!"
 
-def get(url, auth=None):
-    response = requests.get('url')
+# Handle to make HTTP POST Request and return a JSON output
+def postRequest(url, auth=None, body=None):
+    response = requests.post(url, data=body)
     return response.json()
+
+# Handle to make HTTP Delete Requests
+def deleteRequest(url, auth=None):
+    response = requests.delete(url)
+    return response.json()
+
+def main():
+    testurl  = "http://isis.poly.edu/~marcbudofsky/cs6963-fall2016/URLs"
+    testurl2 = "http://whois.arin.net/rest/poc/NYU-ARIN"
+    print getRequest(testurl)
+    print getRequest(testurl2, {"Accept": "application/json"})
+
+main()
 
 
