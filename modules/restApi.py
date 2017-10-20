610aacab9c93648937cbd212ecc64a26a3a0cf2b
Srinivas <spg349@nyu.edu>
2017-10-19 22:16:28 -0400

    get request fixes

--- modules/restApi.py
+++ modules/restApi.py
@@ -15,19 +15,26 @@
 #######################################################################################
 # Import Statements for Libraries
 import requests # Rest Api Library
+import json
 
 # Handle to make HTTP GET Request and return a JSON output
-def getRequest(url, headers=None, auth=None, ):
-    if headers:
-        response = requests.get(url, headers =headers)
-    response = requests.get(url)
+def getRequest(url, headers=None, type=None, auth=None):
+    if not headers:
+        headers = {'Content-Type':'text/html'}
+    if str(type).lower() == "json":
+        headers = {'Content-Type':'application/json', 'Accept':'application/json'}
+        response = requests.get(url, headers=headers)
+    else:
+        response = requests.get(url, headers)
+    if auth:
+        response = requests.get(url, auth)
     try:
         return response.json()
     except ValueError as e:
         if str(e).lower().strip() == "no json object could be decoded":
             return response.text
         else:
-            print "Get Request to the url "+url+" failed!"
+            print "Get Request to the url "+str(url)+" failed!"
 
 # Handle to make HTTP POST Request and return a JSON output
 def postRequest(url, auth=None, body=None):
@@ -43,7 +50,7 @@ def main():
     testurl  = "http://isis.poly.edu/~marcbudofsky/cs6963-fall2016/URLs"
     testurl2 = "http://whois.arin.net/rest/poc/NYU-ARIN"
     print getRequest(testurl)
-    print getRequest(testurl2, {"Accept": "application/json"})
+    print getRequest(testurl2, None, "json")
 
 main()
 
