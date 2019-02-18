b1d550f27569518223f7847e672977ef7357618d
srinivas11789 <spg349@nyu.edu>
2019-02-17 21:04:31 -0800

    more unittests and fixes

---
+++ test/test_full_work_flow.py
@@ -0,0 +1,12 @@
+# Main file execution
+import sys
+
+if sys.path[0]:
+   sys.path.insert(0,sys.path[0]+"/../urlrecon/")
+else:
+   sys.path.insert(0,"/../urlrecon/")
+
+import subprocess
+
+def test_work_flow():
+    assert "Successfully" in (subprocess.check_output(["python", sys.path[0]+"main.py", "f", "examples/url_to_test.txt"]))
